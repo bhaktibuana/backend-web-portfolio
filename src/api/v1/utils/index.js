0b9c8878aa21e10b61452d6053b1dc11029c635b
Bhakti Mega Buana <bhaktimb@ACSET.CO>
2023-03-27 14:11:30 +0700

    create generate id and hash password utilities

---
+++ src/api/v1/utils/index.js
@@ -0,0 +1,7 @@
+const generateIdUtil = require("./generateId.util");
+const hashPasswordUtil = require("./hashPassword.util");
+
+module.exports = {
+  generateId: generateIdUtil,
+  hashPassword: hashPasswordUtil,
+};
